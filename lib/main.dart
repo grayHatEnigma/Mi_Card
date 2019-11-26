47d2892d72982c42864a696eccdec80199378fe3
grayHatEnigma <salama92work@gmail.com>
2019-11-26 05:11:17 +0200

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,66 @@ class MyApp extends StatelessWidget {
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       color: Colors.white,
+                      fontSize: 27,
+                      fontFamily: 'Pacifico'),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Flutter Developer'.toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.teal[100],
                       fontSize: 20,
-                      fontStyle: FontStyle.italic),
-                )
+                      wordSpacing: 7,
+                      letterSpacing: 4,
+                      fontFamily: 'SourceSansPro'),
+                ),
+                SizedBox(
+                  height: 25,
+                  width: 140,
+                  child: Divider(
+                    thickness: 0.75,
+                    color: Colors.teal[50],
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+201558696799',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal[500],
+                          fontStyle: FontStyle.italic,
+                          fontFamily: 'SourceSansPro'),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'salama92work@gmail.com',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.teal[500],
+                            fontStyle: FontStyle.italic,
+                            fontFamily: 'SourceSansPro'),
+                      ),
+                    ))
               ],
             ),
           ),
